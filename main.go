f3ccb66ef296891283b4f3debe627020bbc014ef
Rahul Nema <43380701+rahulnema0411@users.noreply.github.com>
2020-11-09 01:26:01 +0530

    added get request

--- main.go
+++ main.go
@@ -1,32 +1,61 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
-	"time"
-
-	"go.mongodb.org/mongo-driver/bson/primitive"
+	"text/template"
 )
 
 type Article struct {
-	ID                primitive.ObjectID
-	Title             string
-	Subtitle          string
-	Content           string
-	Created_TimeStamp time.Time
+	ID       string `json:"id"`
+	Title    string `json:"Title"`
+	Subtitle string `json:"desc"`
+	Content  string `json:"content"`
 }
 
-func main() {
-	fmt.Println("Starting server")
-	http.HandleFunc("/", HelloServer)
-	http.HandleFunc("/article", GetArticleEndPoint)
+var Articles []Article
+
+func homePage(response http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(response, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
+}
+
+func returnAllArticles(response http.ResponseWriter, request *http.Request) {
+	fmt.Println("Endpoint Hit: returnAllArticles")
+	json.NewEncoder(response).Encode(Articles)
+}
+
+func createArticle(response http.ResponseWriter, request *http.Request) {
+	fmt.Println("methods", request.Method)
+	if request.Method == "GET" {
+		t, _ := template.ParseFiles("createArticle.html")
+		t.Execute(response, nil)
+	} else {
+		request.ParseForm()
+		// logic part of log in
+		fmt.Println("ID:", request.Form["ID"])
+		fmt.Println("Title:", request.Form["Title"])
+		fmt.Println("Subtitle:", request.Form["Subtitle"])
+		fmt.Println("Content:", request.Form["Content"])
+	}
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/articles", returnAllArticles)
+	http.HandleFunc("/articles", createArticle)
 	http.ListenAndServe(":27017", nil)
 }
 
-func HelloServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Just saying hello, %s!", r.URL.Path[1:])
+func handleResponse() {
+
 }
 
-func GetArticleEndPoint(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "The article end point, %s!", r.URL.Path[1:])
+func main() {
+	Articles = []Article{
+		Article{ID: "1", Title: "Hello", Subtitle: "Article Description", Content: "Article Content"},
+		Article{ID: "2", Title: "Hello 2", Subtitle: "Article Description", Content: "Article Content"},
+	}
+	handleRequests()
 }
