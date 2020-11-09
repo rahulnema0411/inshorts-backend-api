68e4708b29a815d0860f549c978926215daf31f4
Rahul Nema <43380701+rahulnema0411@users.noreply.github.com>
2020-11-09 15:06:03 +0530

    added GET/POST route and query route

--- main.go
+++ main.go
@@ -3,14 +3,15 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
-	"text/template"
 )
 
 type Article struct {
 	ID       string `json:"id"`
 	Title    string `json:"Title"`
-	Subtitle string `json:"desc"`
+	Subtitle string `json:"subtitle"`
 	Content  string `json:"content"`
 }
 
@@ -21,41 +22,54 @@ func homePage(response http.ResponseWriter, request *http.Request) {
 	fmt.Println("Endpoint Hit: homePage")
 }
 
-func returnAllArticles(response http.ResponseWriter, request *http.Request) {
-	fmt.Println("Endpoint Hit: returnAllArticles")
-	json.NewEncoder(response).Encode(Articles)
-}
-
-func createArticle(response http.ResponseWriter, request *http.Request) {
+func articles(response http.ResponseWriter, request *http.Request) {
 	fmt.Println("methods", request.Method)
 	if request.Method == "GET" {
-		t, _ := template.ParseFiles("createArticle.html")
-		t.Execute(response, nil)
-	} else {
-		request.ParseForm()
-		// logic part of log in
-		fmt.Println("ID:", request.Form["ID"])
-		fmt.Println("Title:", request.Form["Title"])
-		fmt.Println("Subtitle:", request.Form["Subtitle"])
-		fmt.Println("Content:", request.Form["Content"])
+		json.NewEncoder(response).Encode(Articles)
+	} else if request.Method == "POST" {
+		body, err := ioutil.ReadAll(request.Body)
+		if err != nil {
+			panic(err)
+		}
+		log.Println(string(body))
+		var article Article
+		json.Unmarshal([]byte(body), &article)
+		fmt.Println("Printing response")
+		fmt.Println(article.ID)
+		fmt.Println(article.Title)
+		fmt.Println(article.Subtitle)
+		fmt.Println(article.Content)
 	}
+}
+
+func search(response http.ResponseWriter, request *http.Request) {
+	fmt.Println("GET params were:", request.URL.Query())
+	query := request.URL.Query().Get("q")
+	fmt.Fprintf(response, "Welcome to the searchPage!")
+	fmt.Println(query)
+}
+
+func getArticle(response http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(response, "Welcome to the getArticle page!")
 }
 
 func handleRequests() {
 	http.HandleFunc("/", homePage)
-	http.HandleFunc("/articles", returnAllArticles)
-	http.HandleFunc("/articles", createArticle)
+	http.HandleFunc("/articles", articles)
+	http.HandleFunc("/articles/search", search)
+	http.HandleFunc("/articles/{id}", getArticle)
 	http.ListenAndServe(":27017", nil)
-}
-
-func handleResponse() {
-
 }
 
 func main() {
 	Articles = []Article{
-		Article{ID: "1", Title: "Hello", Subtitle: "Article Description", Content: "Article Content"},
+		Article{ID: "1", Title: "Hello 2", Subtitle: "Article Description", Content: "Article Content"},
 		Article{ID: "2", Title: "Hello 2", Subtitle: "Article Description", Content: "Article Content"},
+		Article{ID: "3", Title: "Hello 2", Subtitle: "Article Description", Content: "Article Content"},
+		Article{ID: "4", Title: "Hello 2", Subtitle: "Article Description", Content: "Article Content"},
+		Article{ID: "5", Title: "Hello 2", Subtitle: "Article Description", Content: "Article Content"},
+		Article{ID: "6", Title: "Hello 2", Subtitle: "Article Description", Content: "Article Content"},
+		Article{ID: "7", Title: "Hello 2", Subtitle: "Article Description", Content: "Article Content"},
 	}
 	handleRequests()
 }
